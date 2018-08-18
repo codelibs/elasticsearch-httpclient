4d49f84a04e8032073e9f38583b829f6a11c2f25
Shinsuke Sugaya <shinsuke@apache.org>
2018-08-18 22:07:20 +0900

    add request parameters

--- src/main/java/org/codelibs/elasticsearch/client/action/HttpIndexAction.java
+++ src/main/java/org/codelibs/elasticsearch/client/action/HttpIndexAction.java
@@ -17,16 +17,22 @@ package org.codelibs.elasticsearch.client.action;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
 
+import org.codelibs.curl.CurlRequest;
 import org.codelibs.elasticsearch.client.HttpClient;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.DocWriteRequest.OpType;
 import org.elasticsearch.action.index.IndexAction;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.support.ActiveShardCount;
+import org.elasticsearch.action.support.WriteRequest.RefreshPolicy;
 import org.elasticsearch.common.xcontent.XContentHelper;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.VersionType;
 
 public class HttpIndexAction extends HttpAction {
 
@@ -44,15 +50,47 @@ public class HttpIndexAction extends HttpAction {
         } catch (final IOException e) {
             throw new ElasticsearchException("Failed to parse a request.", e);
         }
-        client.getCurlRequest(PUT, "/" + request.type() + "/" + request.id(), request.index()).param("routing", request.routing())
-                .body(source).execute(response -> {
-                    try (final InputStream in = response.getContentAsStream()) {
-                        final XContentParser parser = createParser(in);
-                        final IndexResponse indexResponse = IndexResponse.fromXContent(parser);
-                        listener.onResponse(indexResponse);
-                    } catch (final Exception e) {
-                        listener.onFailure(e);
-                    }
-                }, listener::onFailure);
+        getCurlRequest(request).body(source).execute(response -> {
+            try (final InputStream in = response.getContentAsStream()) {
+                final XContentParser parser = createParser(in);
+                final IndexResponse indexResponse = IndexResponse.fromXContent(parser);
+                listener.onResponse(indexResponse);
+            } catch (final Exception e) {
+                listener.onFailure(e);
+            }
+        }, listener::onFailure);
+    }
+
+    private CurlRequest getCurlRequest(final IndexRequest request) {
+        // RestIndexAction
+        CurlRequest curlRequest =
+                client.getCurlRequest(OpType.CREATE.equals(request.opType()) ? PUT : POST, "/" + request.type() + "/" + request.id(),
+                        request.index());
+        if (request.routing() != null) {
+            curlRequest.param("routing", request.routing());
+        }
+        if (request.parent() != null) {
+            curlRequest.param("parent", request.parent());
+        }
+        if (request.getPipeline() != null) {
+            curlRequest.param("pipeline", request.getPipeline());
+        }
+        if (request.timeout() != null) {
+            curlRequest.param("timeout", request.timeout().toString());
+        }
+        if (!RefreshPolicy.NONE.equals(request.getRefreshPolicy())) {
+            curlRequest.param("refresh", request.getRefreshPolicy().getValue());
+        }
+        if (request.version() >= 0) {
+            curlRequest.param("version", Long.toString(request.version()));
+        }
+        if (!VersionType.INTERNAL.equals(request.versionType())) {
+            curlRequest.param("version_type", request.versionType().name().toLowerCase(Locale.ROOT));
+        }
+        if (!ActiveShardCount.DEFAULT.equals(request.waitForActiveShards())) {
+            curlRequest.param("wait_for_active_shards", request.waitForActiveShards().toString());
+        }
+        curlRequest.param("op_type", request.opType().getLowercase());
+        return curlRequest;
     }
 }
