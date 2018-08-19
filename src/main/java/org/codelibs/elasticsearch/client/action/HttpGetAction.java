40d438bdb0c75492e4769ec17cad497acb77ff2a
Shinsuke Sugaya <shinsuke@apache.org>
2018-08-19 09:24:55 +0900

    add request parameters

--- src/main/java/org/codelibs/elasticsearch/client/action/HttpGetAction.java
+++ src/main/java/org/codelibs/elasticsearch/client/action/HttpGetAction.java
@@ -16,13 +16,16 @@
 package org.codelibs.elasticsearch.client.action;
 
 import java.io.InputStream;
+import java.util.Locale;
 
+import org.codelibs.curl.CurlRequest;
 import org.codelibs.elasticsearch.client.HttpClient;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.get.GetAction;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.index.VersionType;
 
 public class HttpGetAction extends HttpAction {
 
@@ -34,15 +37,44 @@ public class HttpGetAction extends HttpAction {
     }
 
     public void execute(final GetRequest request, final ActionListener<GetResponse> listener) {
-        client.getCurlRequest(GET, "/" + request.type() + "/" + request.id(), request.index()).param("routing", request.routing())
-                .param("preference", request.preference()).execute(response -> {
-                    try (final InputStream in = response.getContentAsStream()) {
-                        final XContentParser parser = createParser(in);
-                        final GetResponse getResponse = GetResponse.fromXContent(parser);
-                        listener.onResponse(getResponse);
-                    } catch (final Exception e) {
-                        listener.onFailure(e);
-                    }
-                }, listener::onFailure);
+        getCurlRequest(request).execute(response -> {
+            try (final InputStream in = response.getContentAsStream()) {
+                final XContentParser parser = createParser(in);
+                final GetResponse getResponse = GetResponse.fromXContent(parser);
+                listener.onResponse(getResponse);
+            } catch (final Exception e) {
+                listener.onFailure(e);
+            }
+        }, listener::onFailure);
+    }
+
+    private CurlRequest getCurlRequest(final GetRequest request) {
+        // RestGetAction
+        final CurlRequest curlRequest = client.getCurlRequest(GET, "/" + request.type() + "/" + request.id(), request.index());
+        if (request.refresh()) {
+            curlRequest.param("refresh", "true");
+        }
+        if (request.routing() != null) {
+            curlRequest.param("routing", request.routing());
+        }
+        if (request.parent() != null) {
+            curlRequest.param("parent", request.parent());
+        }
+        if (request.preference() != null) {
+            curlRequest.param("preference", request.preference());
+        }
+        if (request.realtime()) {
+            curlRequest.param("realtime", "true");
+        }
+        if (request.storedFields() != null) {
+            curlRequest.param("stored_fields", String.join(",", request.storedFields()));
+        }
+        if (request.version() >= 0) {
+            curlRequest.param("version", Long.toString(request.version()));
+        }
+        if (!VersionType.INTERNAL.equals(request.versionType())) {
+            curlRequest.param("version_type", request.versionType().name().toLowerCase(Locale.ROOT));
+        }
+        return curlRequest;
     }
 }
