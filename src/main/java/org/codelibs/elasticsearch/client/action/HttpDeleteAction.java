0ad234ee3912ca9d016fdf0512d12228705c5d6a
Shinsuke Sugaya <shinsuke@apache.org>
2018-08-19 09:44:33 +0900

    add request parameters

--- src/main/java/org/codelibs/elasticsearch/client/action/HttpDeleteAction.java
+++ src/main/java/org/codelibs/elasticsearch/client/action/HttpDeleteAction.java
@@ -16,13 +16,18 @@
 package org.codelibs.elasticsearch.client.action;
 
 import java.io.InputStream;
+import java.util.Locale;
 
+import org.codelibs.curl.CurlRequest;
 import org.codelibs.elasticsearch.client.HttpClient;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.delete.DeleteAction;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.support.ActiveShardCount;
+import org.elasticsearch.action.support.WriteRequest.RefreshPolicy;
 import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.index.VersionType;
 
 public class HttpDeleteAction extends HttpAction {
 
@@ -34,15 +39,41 @@ public class HttpDeleteAction extends HttpAction {
     }
 
     public void execute(final DeleteRequest request, final ActionListener<DeleteResponse> listener) {
-        client.getCurlRequest(DELETE, "/" + request.type() + "/" + request.id(), request.index()).param("routing", request.routing())
-                .param("version", String.valueOf(request.version())).execute(response -> {
-                    try (final InputStream in = response.getContentAsStream()) {
-                        final XContentParser parser = createParser(in);
-                        final DeleteResponse deleteResponse = DeleteResponse.fromXContent(parser);
-                        listener.onResponse(deleteResponse);
-                    } catch (final Exception e) {
-                        listener.onFailure(e);
-                    }
-                }, listener::onFailure);
+        getCurlRequest(request).execute(response -> {
+            try (final InputStream in = response.getContentAsStream()) {
+                final XContentParser parser = createParser(in);
+                final DeleteResponse deleteResponse = DeleteResponse.fromXContent(parser);
+                listener.onResponse(deleteResponse);
+            } catch (final Exception e) {
+                listener.onFailure(e);
+            }
+        }, listener::onFailure);
+    }
+
+    protected CurlRequest getCurlRequest(final DeleteRequest request) {
+        // RestDeleteAction
+        final CurlRequest curlRequest = client.getCurlRequest(DELETE, "/" + request.type() + "/" + request.id(), request.index());
+        if (request.routing() != null) {
+            curlRequest.param("routing", request.routing());
+        }
+        if (request.parent() != null) {
+            curlRequest.param("parent", request.parent());
+        }
+        if (request.timeout() != null) {
+            curlRequest.param("timeout", request.timeout().toString());
+        }
+        if (!RefreshPolicy.NONE.equals(request.getRefreshPolicy())) {
+            curlRequest.param("refresh", request.getRefreshPolicy().getValue());
+        }
+        if (request.version() >= 0) {
+            curlRequest.param("version", Long.toString(request.version()));
+        }
+        if (!VersionType.INTERNAL.equals(request.versionType())) {
+            curlRequest.param("version_type", request.versionType().name().toLowerCase(Locale.ROOT));
+        }
+        if (!ActiveShardCount.DEFAULT.equals(request.waitForActiveShards())) {
+            curlRequest.param("wait_for_active_shards", request.waitForActiveShards().toString());
+        }
+        return curlRequest.param("routing", request.routing()).param("version", String.valueOf(request.version()));
     }
 }
