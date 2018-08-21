c7e74681a3e25f4e76af371a7efeb3fb7a0dc72c
igarash1 <kigrsk@gmail.com>
2018-08-21 17:07:07 +0900

    fix MultiSearchAction

--- src/main/java/org/codelibs/elasticsearch/client/action/HttpMultiSearchAction.java
+++ src/main/java/org/codelibs/elasticsearch/client/action/HttpMultiSearchAction.java
@@ -59,7 +59,9 @@ public class HttpMultiSearchAction extends HttpAction {
     protected CurlRequest getCurlRequest(final MultiSearchRequest request) {
         // RestMultiSearchAction
         final CurlRequest curlRequest = client.getCurlRequest(GET, ContentType.X_NDJSON, "/_msearch");
-        curlRequest.param("max_concurrent_searches", Integer.toString(request.maxConcurrentSearchRequests()));
+        if(request.maxConcurrentSearchRequests() > 0) {
+            curlRequest.param("max_concurrent_searches", Integer.toString(request.maxConcurrentSearchRequests()));
+        }
         return curlRequest;
     }
 }
