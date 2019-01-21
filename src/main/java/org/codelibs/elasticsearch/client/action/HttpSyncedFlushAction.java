ac86790b6289dcfedca50d5d8decad52c6f4605c
Shinsuke Sugaya <shinsuke@apache.org>
2019-01-22 06:26:39 +0900

    update license

--- src/main/java/org/codelibs/elasticsearch/client/action/HttpSyncedFlushAction.java
+++ src/main/java/org/codelibs/elasticsearch/client/action/HttpSyncedFlushAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 CodeLibs Project and the Others.
+ * Copyright 2012-2019 CodeLibs Project and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -152,8 +152,8 @@ public class HttpSyncedFlushAction extends HttpAction {
         return shardsSyncedFlushResults;
     }
 
-    protected void parseFailuresField(final XContentParser parser, final Index index, final List<ShardsSyncedFlushResult> shardsSyncedFlushResults, final int total)
-            throws IOException {
+    protected void parseFailuresField(final XContentParser parser, final Index index,
+            final List<ShardsSyncedFlushResult> shardsSyncedFlushResults, final int total) throws IOException {
         Token token;
         while ((token = parser.nextToken()) != Token.END_ARRAY) {
             ensureExpectedToken(Token.START_OBJECT, token, parser::getTokenLocation);
